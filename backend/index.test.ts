f402f4685cdbe465f2f9a33a5a37c2f8d24341dc
Mustafa "Moose" Paksoy <paksoy@gmail.com>
2024-10-08 01:01:06 -0700

    wip

--- backend/index.test.ts
+++ backend/index.test.ts
@@ -14,11 +14,20 @@ describe("dummy test", () => {
     expect(validation.success).toBe(true);
   });
 
-  it("sync typia validate", async () => {
+  it("async typia validate", async () => {
     interface Schema {
       name: string;
     }
     const data = { name: "foo" };
     assertGuardEquals<Schema>(data);
+  });
+
+  it("sync typia validate", async () => {
+    interface Schema {
+      name: string;
+      other: number;
+    }
+    const data = { name: "foo" };
+    expect(() => assertGuardEquals<Schema>(data)).toThrow();
   });
 });
